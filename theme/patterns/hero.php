86d5da4fbda1d11a6b20e73cdddfbf0421294ad6
Agnieszka Palmowska <a.palmowska@freshpixels.pl>
2026-04-06 20:47:56 +0200

    Add export.wxr file and update release workflow to include it; remove hello pattern and add hero pattern

---
+++ theme/patterns/hero.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Title: Hero Pattern
+ * Slug: playground-test-theme/hero
+ * Categories: featured
+ */
+?>
+<!-- wp:paragraph -->
+<p>This is a hero pattern with dummy text.</p>
+<!-- /wp:paragraph -->

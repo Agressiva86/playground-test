930bb20681aa3c8ead9dc581c7b0773e55ac905d
Agnieszka Palmowska <a.palmowska@freshpixels.pl>
2026-04-16 13:03:03 +0200

    Hero pattern v2 fixed

---
+++ plugin/patterns/hero-fixed.php
@@ -0,0 +1,73 @@
+<!-- wp:group {"metadata":{"name":"Hero Pattern v2 - fixed"},"align":"wide","style":{"spacing":{"blockGap":"var:preset|spacing|50"}},"layout":{"type":"constrained"}} -->
+<div class="wp-block-group alignwide"><!-- wp:columns {"align":"wide","style":{"spacing":{"padding":{"right":"0","left":"0"}}}} -->
+<div class="wp-block-columns alignwide" style="padding-right:0;padding-left:0"><!-- wp:column {"verticalAlignment":"bottom"} -->
+<div class="wp-block-column is-vertically-aligned-bottom"><!-- wp:group {"style":{"spacing":{"margin":{"top":"0","bottom":"0"},"blockGap":"var:preset|spacing|20"}},"layout":{"type":"default"}} -->
+<div class="wp-block-group" style="margin-top:0;margin-bottom:0"><!-- wp:paragraph -->
+<p><strong>A place for the over-title</strong></p>
+<!-- /wp:paragraph -->
+
+<!-- wp:heading {"level":1,"className":"is-style-default"} -->
+<h1 class="wp-block-heading is-style-default"><strong>Figma plugin share auto outline background scale.</strong></h1>
+<!-- /wp:heading --></div>
+<!-- /wp:group -->
+
+<!-- wp:paragraph -->
+<p>Plugin strikethrough mask figma boolean hand plugin group font main. Asset figjam bullet thumbnail share italic overflow follower. Pencil layer boolean duplicate rectangle. Inspect hand rectangle object draft blur library.</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:group {"layout":{"type":"flex","flexWrap":"nowrap"}} -->
+<div class="wp-block-group"><!-- wp:buttons -->
+<div class="wp-block-buttons"><!-- wp:button {"style":{"border":{"radius":{"topLeft":"0px","topRight":"0px","bottomLeft":"0px","bottomRight":"0px"}},"typography":{"textTransform":"uppercase"}},"fontSize":"medium"} -->
+<div class="wp-block-button"><a class="wp-block-button__link has-medium-font-size has-custom-font-size wp-element-button" style="border-top-left-radius:0px;border-top-right-radius:0px;border-bottom-left-radius:0px;border-bottom-right-radius:0px;text-transform:uppercase"><strong>Button label</strong></a></div>
+<!-- /wp:button -->
+
+<!-- wp:button {"backgroundColor":"base","textColor":"contrast","className":"is-style-outline","style":{"border":{"radius":{"topLeft":"0px","topRight":"0px","bottomLeft":"0px","bottomRight":"0px"},"width":"2px"},"elements":{"link":{"color":{"text":"var:preset|color|contrast"}}},"typography":{"textTransform":"uppercase"}},"fontSize":"medium"} -->
+<div class="wp-block-button is-style-outline"><a class="wp-block-button__link has-contrast-color has-base-background-color has-text-color has-background has-link-color has-medium-font-size has-custom-font-size wp-element-button" style="border-width:2px;border-top-left-radius:0px;border-top-right-radius:0px;border-bottom-left-radius:0px;border-bottom-right-radius:0px;text-transform:uppercase"><strong>Button label 2</strong></a></div>
+<!-- /wp:button --></div>
+<!-- /wp:buttons --></div>
+<!-- /wp:group --></div>
+<!-- /wp:column -->
+
+<!-- wp:column {"verticalAlignment":"bottom"} -->
+<div class="wp-block-column is-vertically-aligned-bottom"><!-- wp:cover {"url":"http://extendify-playground.test/wp-content/uploads/2026/04/beautiful-female-office-worker-carrying-out-administrative-work-company-scaled.jpg","id":12,"dimRatio":0,"minHeight":500,"isDark":false} -->
+<div class="wp-block-cover is-light" style="min-height:500px"><img class="wp-block-cover__image-background wp-image-12" alt="" src="__PLUGIN_URL__beautiful-female-office-worker-carrying-out-administrative-work-company-scaled.jpg" data-object-fit="cover"/><span aria-hidden="true" class="wp-block-cover__background has-background-dim-0 has-background-dim"></span><div class="wp-block-cover__inner-container"><!-- wp:paragraph {"align":"center","fontSize":"large"} -->
+<p class="has-text-align-center has-large-font-size"></p>
+<!-- /wp:paragraph --></div></div>
+<!-- /wp:cover --></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns -->
+
+<!-- wp:columns {"verticalAlignment":"bottom","align":"wide"} -->
+<div class="wp-block-columns alignwide are-vertically-aligned-bottom"><!-- wp:column {"verticalAlignment":"bottom"} -->
+<div class="wp-block-column is-vertically-aligned-bottom"><!-- wp:cover {"url":"http://extendify-playground.test/wp-content/uploads/2026/04/stylish-female-web-programmers-talking-about-work-while-spending-time-office-indoor-portrait-african-woman-headphones-asian-worker-using-computers-scaled.jpg","id":26,"dimRatio":0,"minHeight":250,"isDark":false} -->
+<div class="wp-block-cover is-light" style="min-height:250px"><img class="wp-block-cover__image-background wp-image-26" alt="" src="__PLUGIN_URL__stylish-female-web-programmers-talking-about-work-while-spending-time-office-indoor-portrait-african-woman-headphones-asian-worker-using-computers-scaled.jpg" data-object-fit="cover"/><span aria-hidden="true" class="wp-block-cover__background has-background-dim-0 has-background-dim"></span><div class="wp-block-cover__inner-container"><!-- wp:paragraph {"align":"center","fontSize":"large"} -->
+<p class="has-text-align-center has-large-font-size"></p>
+<!-- /wp:paragraph --></div></div>
+<!-- /wp:cover --></div>
+<!-- /wp:column -->
+
+<!-- wp:column {"verticalAlignment":"bottom"} -->
+<div class="wp-block-column is-vertically-aligned-bottom"><!-- wp:group {"layout":{"type":"flex","orientation":"vertical"}} -->
+<div class="wp-block-group"><!-- wp:heading -->
+<h2 class="wp-block-heading"><strong>40+</strong></h2>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph -->
+<p>Plugin strikethrough mask figma boolean hand plugin group font main.</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group --></div>
+<!-- /wp:column -->
+
+<!-- wp:column {"verticalAlignment":"bottom"} -->
+<div class="wp-block-column is-vertically-aligned-bottom"><!-- wp:group {"layout":{"type":"flex","orientation":"vertical"}} -->
+<div class="wp-block-group"><!-- wp:heading -->
+<h2 class="wp-block-heading"><strong>120+</strong></h2>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph -->
+<p>Pencil layer boolean duplicate rectangle. Inspect hand rectangle object draft blur library.</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group --></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns --></div>
+<!-- /wp:group -->

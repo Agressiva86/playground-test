150de944c93c22d4fa1b7c48cf894bbc2b885651
Agnieszka Palmowska <a.palmowska@freshpixels.pl>
2026-04-16 14:04:28 +0200

    New pattern - Hero Pattern v1

---
+++ plugin/patterns/hero-1.php
@@ -0,0 +1,60 @@
+<?php
+/**
+ * Title: Hero Pattern v1
+ * Slug: playground-patterns/hero-1
+ * Categories: playground-patterns
+ */
+?>
+<!-- wp:group {"metadata":{"name":"Hero Pattern v1"},"align":"wide","style":{"spacing":{"blockGap":"var:preset|spacing|50"}},"layout":{"type":"constrained"}} -->
+<div class="wp-block-group alignwide"><!-- wp:columns -->
+<div class="wp-block-columns"><!-- wp:column -->
+<div class="wp-block-column"><!-- wp:paragraph {"align":"center"} -->
+<p class="has-text-align-center"><strong>place for the over-title</strong></p>
+<!-- /wp:paragraph -->
+
+<!-- wp:heading {"textAlign":"center","level":1} -->
+<h1 class="wp-block-heading has-text-align-center"><strong>Figma plugin share auto outline background scale.</strong></h1>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph {"align":"center"} -->
+<p class="has-text-align-center">Plugin strikethrough mask figma boolean hand plugin group font main. Asset figjam bullet thumbnail share italic overflow follower. Pencil layer boolean duplicate rectangle. Inspect hand rectangle object draft blur library.</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:buttons {"layout":{"type":"flex","justifyContent":"center"}} -->
+<div class="wp-block-buttons"><!-- wp:button {"style":{"border":{"radius":{"topLeft":"0px","topRight":"0px","bottomLeft":"0px","bottomRight":"0px"}}}} -->
+<div class="wp-block-button"><a class="wp-block-button__link wp-element-button" style="border-top-left-radius:0px;border-top-right-radius:0px;border-bottom-left-radius:0px;border-bottom-right-radius:0px">Button label</a></div>
+<!-- /wp:button -->
+
+<!-- wp:button {"className":"is-style-outline","style":{"border":{"radius":{"topLeft":"0px","topRight":"0px","bottomLeft":"0px","bottomRight":"0px"}}}} -->
+<div class="wp-block-button is-style-outline"><a class="wp-block-button__link wp-element-button" style="border-top-left-radius:0px;border-top-right-radius:0px;border-bottom-left-radius:0px;border-bottom-right-radius:0px">Button label</a></div>
+<!-- /wp:button --></div>
+<!-- /wp:buttons --></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns -->
+
+<!-- wp:columns {"align":"wide"} -->
+<div class="wp-block-columns alignwide"><!-- wp:column -->
+<div class="wp-block-column"><!-- wp:cover {"url":"__PLUGIN_URL__joyful-african-web-developer-working-new-project-while-listening-music-white-headphones-black-female-designer-doing-her-job-office-with-talking-colleagues.jpg","id":139,"dimRatio":0,"minHeight":550,"isDark":false} -->
+<div class="wp-block-cover is-light" style="min-height:550px"><img class="wp-block-cover__image-background wp-image-139" alt="" src="__PLUGIN_URL__joyful-african-web-developer-working-new-project-while-listening-music-white-headphones-black-female-designer-doing-her-job-office-with-talking-colleagues.jpg" data-object-fit="cover"/><span aria-hidden="true" class="wp-block-cover__background has-background-dim-0 has-background-dim"></span><div class="wp-block-cover__inner-container"><!-- wp:paragraph {"align":"center","fontSize":"large"} -->
+<p class="has-text-align-center has-large-font-size"></p>
+<!-- /wp:paragraph --></div></div>
+<!-- /wp:cover --></div>
+<!-- /wp:column -->
+
+<!-- wp:column -->
+<div class="wp-block-column"><!-- wp:cover {"url":"__PLUGIN_URL__elegant-african-web-designer-doing-her-job-office-with-smile-attractive-black-woman-white-headphones-working-call-center-sitting-near-computer.jpg","id":140,"dimRatio":0,"minHeight":550,"isDark":false,"style":{"spacing":{"margin":{"top":"var:preset|spacing|50","bottom":"var:preset|spacing|50"}}}} -->
+<div class="wp-block-cover is-light" style="margin-top:var(--wp--preset--spacing--50);margin-bottom:var(--wp--preset--spacing--50);min-height:550px"><img class="wp-block-cover__image-background wp-image-140" alt="" src="__PLUGIN_URL__elegant-african-web-designer-doing-her-job-office-with-smile-attractive-black-woman-white-headphones-working-call-center-sitting-near-computer.jpg" data-object-fit="cover"/><span aria-hidden="true" class="wp-block-cover__background has-background-dim-0 has-background-dim"></span><div class="wp-block-cover__inner-container"><!-- wp:paragraph {"align":"center","fontSize":"large"} -->
+<p class="has-text-align-center has-large-font-size"></p>
+<!-- /wp:paragraph --></div></div>
+<!-- /wp:cover --></div>
+<!-- /wp:column -->
+
+<!-- wp:column -->
+<div class="wp-block-column"><!-- wp:cover {"url":"__PLUGIN_URL__stylish-female-web-programmers-talking-about-work-while-spending-time-office-indoor-portrait-african-woman-headphones-asian-worker-using-computers.jpg","id":141,"dimRatio":0,"minHeight":550,"isDark":false} -->
+<div class="wp-block-cover is-light" style="min-height:550px"><img class="wp-block-cover__image-background wp-image-141" alt="" src="__PLUGIN_URL__stylish-female-web-programmers-talking-about-work-while-spending-time-office-indoor-portrait-african-woman-headphones-asian-worker-using-computers.jpg" data-object-fit="cover"/><span aria-hidden="true" class="wp-block-cover__background has-background-dim-0 has-background-dim"></span><div class="wp-block-cover__inner-container"><!-- wp:paragraph {"align":"center","fontSize":"large"} -->
+<p class="has-text-align-center has-large-font-size"></p>
+<!-- /wp:paragraph --></div></div>
+<!-- /wp:cover --></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns --></div>
+<!-- /wp:group -->
